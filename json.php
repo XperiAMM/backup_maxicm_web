e923abd9a71b90497bff5a3cb0c6acc9567e124c
Andres Mora Montero <andresmora1676@gmail.com>
2015-11-15 18:22:02 -0600

    Revert "test"
    This reverts commit 0ef495b8c013f173784fa89fe78a983a7738853c.

--- json.php
+++
@@ -1,4 +0,0 @@
-
-<br />
-<b>Notice</b>:  Undefined index: device in <b>https://maxicm.com/json.php</b> on line <b>24</b><br />
-{"dev_info":[{"filename":"jagnm"},{"filename":"janice"},{"filename":"mako"},{"filename":"otus"},"downloads":"http:\/\/basketbuild.com\/devs\/MaxiCM\/\/\/.ftpquota"}]}
